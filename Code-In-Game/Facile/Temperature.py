c68272df9a2de9178a8128cfc32f7de103d8b04b
FLDANIEL <florent.fra@gmail.com>
2016-08-19 23:57:17 +0200

    Create Temperature.py

---
+++ Code-In-Game/Facile/Temperature.py
@@ -0,0 +1,31 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())  # the number of temperatures to analyse
+temps = input()  # the n temperatures expressed as integers ranging from -273 to 5526
+
+templist = temps.split(" ")
+print("Value templist :", file=sys.stderr)
+print(templist, file=sys.stderr)
+
+if n == 0:
+    print("0")
+else:
+    mini = 10
+    diff_mini = abs(mini)
+    
+    for t in templist:
+        t = int(t)
+        diff = abs(t)
+
+        if diff < diff_mini:
+            mini = t
+            diff_mini = abs(diff)
+        elif diff == diff_mini and t > mini:
+            mini = t
+            diff_mini = abs(diff)
+            
+print(mini)
